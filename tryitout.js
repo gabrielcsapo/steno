876e13f1f0dfb3e452e08efd17762fc821c555e8
Gabriel Csapo <gabecsapo@gmail.com>
2017-10-05 22:30:38 -0700

    updates demo page

---
+++ tryitout.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: "Steno",
+  description: "A simple SSH shortcut menu for OSX",
+  sourceCodeLink: 'https://github.com/gabrielcsapo/steno',
+  downloadLink: 'https://github.com/gabrielcsapo/steno/releases',
+  icon: './assets/steno.png',
+  demoImage: './assets/example.gif'
+}
